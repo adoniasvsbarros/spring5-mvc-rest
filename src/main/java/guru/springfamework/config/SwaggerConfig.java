d1c7c75ed0f1b1992fa3471fa7100c3cf31dc030
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-08-03 09:24:41 -0300

    Customizing swagger meta data

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -1,17 +1,23 @@
 package guru.springfamework.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { //extends WebMvcConfigurationSupport{
 
 	@Bean
 	public Docket api() {
@@ -20,6 +26,32 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build()
-				.pathMapping("/");
+				.pathMapping("/")
+				.apiInfo(metaData());
 	}
+	
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//	    registry.addResourceHandler("swagger-ui.html")
+//	      .addResourceLocations("classpath:/META-INF/resources/");
+//	 
+//	    registry.addResourceHandler("/webjars/**")
+//	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//	}
+	
+    private ApiInfo metaData(){
+
+        Contact contact = new Contact("Adonias Barros", "https://www.linkedin.com/in/adoniasvsbarros/",
+                "adoniasvicente@hotmail.com");
+
+        return new ApiInfo(
+                "Shop API ",
+                "Created by Adonias Barros",
+                "1.0",
+                "Terms of Service: blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
+    }
 }
